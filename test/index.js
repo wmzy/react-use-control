773a6f84fa56783bdff7b9075faecc4831570775
wmzy <1256573276@qq.com>
2020-08-26 14:48:32 +0800

    test transforms

--- test/index.js
+++ test/index.js
@@ -36,4 +36,57 @@ describe('useModel', function () {
     childResult.current.count.should.equal(3);
     result.current.count.should.equal(3);
   });
+
+  it('should support transforms', function () {
+    const {result} = renderHook(() => {
+      const [model] = useModel(undefined, {
+        count: ([count, setCount]) => [
+          count,
+          (c) =>
+            typeof c === 'function'
+              ? setCount((cc) => c(cc) * 2)
+              : setCount(c * 2)
+        ]
+      });
+      return {model};
+    });
+
+    const {result: childResult} = renderHook(() => {
+      const [, useProp] = useModel(result.current.model);
+      const [count, setCount] = useProp('count', 1);
+      return {count, setCount};
+    });
+
+    childResult.current.count.should.equal(1);
+
+    act(() => {
+      childResult.current.setCount(2);
+    });
+
+    childResult.current.count.should.equal(4);
+  });
+
+  it('should support transforms of array', function () {
+    const countProp = Symbol('count');
+    const {result} = renderHook(() => {
+      const [model] = useModel(undefined, [
+        [countProp, ([count, setCount]) => [count * 2, setCount]]
+      ]);
+      return {model};
+    });
+
+    const {result: childResult} = renderHook(() => {
+      const [, useProp] = useModel(result.current.model);
+      const [count, setCount] = useProp(countProp, 1);
+      return {count, setCount};
+    });
+
+    childResult.current.count.should.equal(2);
+
+    act(() => {
+      childResult.current.setCount(2);
+    });
+
+    childResult.current.count.should.equal(4);
+  });
 });
